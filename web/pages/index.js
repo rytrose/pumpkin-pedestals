6c8f8680e7053b1fc5535c42fe804dfc0076472e
Ryan Rose <rytrose@gmail.com>
2023-02-26 19:17:20 -0500

    initial commit

---
+++ web/pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Pumpkin Pedestals</title>
+        <meta
+          name="description"
+          content="The Rose family glass pumpkin command center"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main></main>
+    </>
+  );
+}
