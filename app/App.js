f8c0afd712ea13444da150c434332f9cf42b7606
Ryan Rose <rytrose@gmail.com>
2023-03-04 00:16:39 -0500

    add 2d graphics lib

--- app/App.js
+++ app/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { useCallback, useState } from "react";
-import { Text, TextInput, View } from "react-native";
+import { Text, TextInput, View, useWindowDimensions } from "react-native";
+import { Canvas, Rect } from "@shopify/react-native-skia";
 
 import { useBlePeripheral } from "./hooks/useBlePeripheral";
 
@@ -15,8 +16,19 @@ export default function App() {
   const [error, connectionStatus, writeToPeripheral] =
     useBlePeripheral(onPeripheralData);
 
+  const { width } = useWindowDimensions();
+  const SIZE = width * 0.8;
+
   return (
     <View className="flex-1 items-center justify-center">
+      <Canvas
+        style={{
+          width: SIZE,
+          height: SIZE,
+        }}
+      >
+        <Rect rect={{ x: 0, y: 0, width: SIZE, height: SIZE }} />
+      </Canvas>
       <Text>Hello, world!</Text>
       <Text>Error: {error || "none"}</Text>
       <Text>Connection Status: {connectionStatus}</Text>
