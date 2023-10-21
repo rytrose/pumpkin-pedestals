3598fb0164c422c755cd3ac9a933b283cf1dd1f4
Ryan Rose <rytrose@gmail.com>
2023-10-20 21:57:07 -0400

    make pedestal browser swipable

--- web/src/components/Carousel.js
+++ web/src/components/Carousel.js
@@ -1,37 +1,57 @@
+import { useSwipeable } from "react-swipeable";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 import { Carousel as MTCarousel, IconButton } from "@material-tailwind/react";
+import { useRef } from "react";
 
 const Carousel = ({ className, items }) => {
+  const handlePrevRef = useRef();
+  const handleNextRef = useRef();
+
+  const swipeHandlers = useSwipeable({
+    onSwipedLeft: (eventData) => {
+      if (handleNextRef.current) handleNextRef.current();
+    },
+    onSwipedRight: (eventData) => {
+      if (handlePrevRef.current) handlePrevRef.current();
+    },
+  });
+
   return (
     <MTCarousel
+      {...swipeHandlers}
       className={className}
-      prevArrow={({ handlePrev, firstIndex }) =>
-        !firstIndex && (
-          <IconButton
-            variant="text"
-            color="gray"
-            size="lg"
-            onClick={handlePrev}
-            className="!absolute top-2/4 left-1 -translate-y-2/4"
-          >
-            <FontAwesomeIcon icon="chevron-left"></FontAwesomeIcon>
-          </IconButton>
-        )
-      }
-      nextArrow={({ handleNext, lastIndex }) =>
-        !lastIndex && (
-          <IconButton
-            variant="text"
-            color="gray"
-            size="lg"
-            onClick={handleNext}
-            className="!absolute top-2/4 !right-1 -translate-y-2/4"
-          >
-            <FontAwesomeIcon icon="chevron-right"></FontAwesomeIcon>
-          </IconButton>
-        )
-      }
+      prevArrow={({ handlePrev, firstIndex }) => {
+        handlePrevRef.current = handlePrev;
+        return (
+          !firstIndex && (
+            <IconButton
+              variant="text"
+              color="gray"
+              size="lg"
+              onClick={handlePrev}
+              className="!absolute top-2/4 left-1 -translate-y-2/4"
+            >
+              <FontAwesomeIcon icon="chevron-left"></FontAwesomeIcon>
+            </IconButton>
+          )
+        );
+      }}
+      nextArrow={({ handleNext, lastIndex }) => {
+        handleNextRef.current = handleNext;
+        return (
+          !lastIndex && (
+            <IconButton
+              variant="text"
+              color="gray"
+              size="lg"
+              onClick={handleNext}
+              className="!absolute top-2/4 !right-1 -translate-y-2/4"
+            >
+              <FontAwesomeIcon icon="chevron-right"></FontAwesomeIcon>
+            </IconButton>
+          )
+        );
+      }}
     >
       {items.map((item, i) => (
         <div key={`carousel-${i}`}>{item}</div>
